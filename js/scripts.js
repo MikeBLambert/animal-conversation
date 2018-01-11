67f957c1d48ac53db0b1d1a5bb51981a066e7522
MikeBLambert <mlamb128@gmail.com>
2018-01-10 20:48:19 -0800

    add images after clicking on h2 tags

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul#dog-response").prepend("<h2>Woof! I love you.</h2>");
     $("h2").first().click(function() {
-      $(this).remove();
+      $(this).after('<img src="img/pumpkin-dog.jpg">')
 
     });
   });
@@ -12,7 +12,8 @@ $(document).ready(function() {
   $("button#woof").click(function() {
     $("ul#cat-response").prepend("<h2>Meow...I'm a stupid cat.</h2>");
     $("h2").first().click(function() {
-      $(this).remove();
+      $(this).after('<img src="img/scared-cat.jpg">')
+
 
       });
   });
